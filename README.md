67dada75a7df0252503f52538ca052301d25539a
Danny Marez <750danny@gmail.com>
2018-09-19 10:00:50 -0600

    initial skills commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# fishinVue
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1> Hello {{ thier_name }}, this is an about page</h1>
+    <p>Hello Vue! This is a Skills list app. My second Vue.js project. Vue is a front end javascript framework.</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Skills', 
+    data() {
+        return {
+            thier_name: this.$route.params.name
+        }
+    }
+}
+</script>
